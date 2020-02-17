e2a60d48a4b4e67e35e42d298691fbecc2c0bbab
Urbanpacker <dev@urbanities.fr>
2020-02-17 18:11:07 +0100

    Nettoyage du fichier des scripts obsolètes et ajout d'un contrôle de données avant de fixer le zoom de la map

--- geolocation.js
+++ geolocation.js
@@ -2,17 +2,6 @@
 
 window.addEventListener('DOMContentLoaded', (function(){
 
-	var testNum = 0;
-
-/*	function test(message){
-		++testNum;
-		let mssg = 'test numero ' + testNum;
-		if(message){
-			mssg = mssg + ' --> ' + '"'+ message +'"';
-		}
-		console.log(mssg);
-	}
-*/
 
 	class SingleLocation{
 		constructor(data){
@@ -32,21 +21,21 @@ window.addEventListener('DOMContentLoaded', (function(){
 				let self = this;
 				if(window.fetch){ // if the navigator supports the relatively recent fetch API
 					fetch(url)
-					.then(function(response){
+					.then((response)=>{
 						return response.json();
 					})
-					.then(function(data){
+					.then((data)=>{
 						self.lg = data.features[0].geometry.coordinates[0],
 					    self.lat = data.features[0].geometry.coordinates[1]
 					})
-					.catch(function(error){
-						console.log(error);
+					.catch((error)=>{
+						//console.log(error);
 					});
 					return;
 				} else{ // if the navigator does not support fetch API, then it uses traditionnal XHR method
 					var request = new XMLHttpRequest();
 					request.open('GET', adress, true);
-					request.onload = function() {
+					request.onload = () => {
 						if (this.status >= 200 && this.status < 400) {
 					    	let data = JSON.parse(this.response);
 					    	self.coords.push({
@@ -57,7 +46,7 @@ window.addEventListener('DOMContentLoaded', (function(){
 					    console.log('The target server has been reached, but it returned an error');
 					  	}
 					};
-					request.onerror = function() {
+					request.onerror = () => {
 					  	console.log('There was a connection error of some sort');
 					};
 					request.send();
@@ -83,8 +72,21 @@ window.addEventListener('DOMContentLoaded', (function(){
 			} 
 
 			this.setCoords = (coords) => {
-				this.coords = coords ;
-				this.zoomPoint = [this.coords[0].lat, this.coords[0].lg];
+				if(coords){
+					this.coords = coords ;
+					let i = 0;
+					let zoomDefined = false; 
+					while(!zoomDefined){
+						if(this.coords[i].lat && this.coords[i].lg){
+							this.zoomPoint = [this.coords[i].lat, this.coords[i].lg];
+							zoomDefined = true;
+						}
+						++i;
+					}
+					return true;
+				}
+
+				return false;
 			}
 
 			this.setOSMMap = () => {
@@ -98,6 +100,7 @@ window.addEventListener('DOMContentLoaded', (function(){
 					let el = this.mapContainer.childNodes ;
 					for(let i = 0, c = el.length; i < c; ++i){
 						if(el[i].nodeType ===  Node.ELEMENT_NODE){
+							console.error("Une carte a déjà été créée dans ce conteneur");
 							return false;
 						}
 					}
@@ -123,180 +126,56 @@ window.addEventListener('DOMContentLoaded', (function(){
 					attribution:'(c)<a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
 				}).addTo(map);
 				// Add a marker to each spot onto the map
-				coords.forEach((el, i, arr)=>{
-					L.marker([el.lat,el.lg]).addTo(map);
-				});
+					coords.forEach((el, i, arr)=>{
+						if(el.lat && el.lg){
+							L.marker([el.lat,el.lg]).addTo(map);
+						}
+					});
 				return true ;
 			}
 		}
 	}
 
-var persons = [
-	{
-		adress : "16 place Bellecour",
-		postCode : "69002"
-	},
-	{
-		adress : "10 place Vauboin",
-		postCode : "69160"
-	},
-	{
-		adress : "50 rue de la République",
-		postCode : "69002"
-	},
-	{
-		adress : "14 cours Charlemagne",
-		postCode : "69002"
-	}
-]
+	var persons = [
+		{
+			adress : "16 place Bellecour",
+			postCode : "69002"
+		},
+		{
+			adress : "10 place Vauboin",
+			postCode : "69160"
+		},
+		{
+			adress : "50 rue de la République",
+			postCode : "69002"
+		},
+		{
+			adress : "14 cours Charlemagne",
+			postCode : "69002"
+		}
+	];
 
-var coords = [];
+	var coords = [];
 
 	for(let i = 0, c = persons.length ; i < c ; ++i){
-		coords.push(new SingleLocation(persons[i]));
-		coords[i].getCoordsFromAdress();	
+		coords[i] = new SingleLocation(persons[i]);
+		coords[i].getCoordsFromAdress();			
 	}
 
+	var maptest = new UrbanMap('mapContainer', 'mapTrigger');
 
-var maptest = new UrbanMap('mapContainer', 'mapTrigger');
-
-
-maptest.mapTrigger.addEventListener('click', e => {
-	maptest.setCoords(coords);
-	maptest.setOSMMap(coords);
-});
+	maptest.mapTrigger.addEventListener('click', e => {
+		maptest.setCoords(coords);
+		setTimeout(() => {
+			maptest.setOSMMap(coords);
+		}, 500);
+	});
 	
 
 
 /*****************************************************
 
 
-	// Defines a common namespace
-	var urban = {
-
-		/* ************** DATA ******* 
-
-		// Defines the minimal geolocation accuracy accepted to display a map and use the coordinates to fill a form
-		minGeolocAccuracy : 30000000000000,
-
-		// Frame in which the map will be displayed
-		//Here is the id of the HTML ELement in which th emap takes place
-		mapContainer : document.getElementById("mapContainer"),
-
-		// Button to click to display the map
-		mapTrigger : document.getElementById("mapTrigger"),
-
-		// body element in HTML DOM
-		
-		/************** METHODS **********
-
-		setOSMMap : (coords, zoomPoint = null) => {
-			if(!coords.isArray && coords.length < 1){
-				return false ;
-			}
-			
-			// Checks that no map has already been set in the container
-			var mapPresent = document.querySelectorAll("#"+urban.mapContainer.id+".leaflet-container");
-
-			if(mapPresent.length > 0){
-				return false;
-			}
-			
-			// If no zoomPoint has been defined, by default the map will zoom on the first spot of the input list
-			if(!zoomPoint){
-				var coordsArray = [] ;
-				coordsArray[0] = coords[0].lat; 
-				coordsArray[1] = coords[0].long; 
-			}
-			
-			// Initializes a OSM Map with options
-			var map = L.map(urban.mapContainer.id).setView(coordsArray, 15);
-			urban.mapContainer.classList.add("map");
-			// Loads the OSM tileLayer (map background) 
-
-			L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-				maxzoom:19,
-				attribution:'(c)<a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-			}).addTo(map);
-			
-			// Add a marker to each spot onto the map
-			coords.forEach((el, i, arr)=>{
-				L.marker([el.lat,el.long]).addTo(map);
-			});
-			return true ;
-		},
-
-		showOSMMapBis : function(coords){
-			
-			var positionFocus = [];
-			var inputAdresse = "16 quai du commerce";
-			var postCode = "69009";
-			var map ;
-
-			inputAdresse = inputAdresse.replace(/ /g, "+");
-			var adress = "https://api-adresse.data.gouv.fr/search/?q="+inputAdresse+"&postcode="+postCode;
-
-
-			if(!window.fetch){ // if the navigator supports fetch API, then it uses it
-				fetch(adress)
-				.then(function(response) {
-					return response.json();
-				})
-				.then(function(json){
-					let positionFocus = json.features[0].geometry.coordinates ;
-					setOSMMap(positionFocus);
-				})
-				.catch(function(error){
-					// console.log(error);
-				});
-				return;
-			} else{ // if the navigator does not support fetch API, then it uses XHR method
-				var request = new XMLHttpRequest();
-				request.open('GET', adress, true);
-				request.onload = function() {
-					if (this.status >= 200 && this.status < 400) {
-				    	let data = JSON.parse(this.response);
-				    	let positionFocus = data.features[0].geometry.coordinates ;
-						setOSMMap(positionFocus);
-					} else {
-				    test('The target server has been reached, but it returned an error');
-				  	}
-				};
-				request.onerror = function() {
-				  	console.log('There was a connection error of some sort');
-				};
-				request.send();
-				return;
-			}
-		},
-
-		// Display an OSM Map with positions markers
-/*
-		showOSMMap : function(){
-		    var positionsList = [] ;
-		    
-		    positionsList.unshift(loadDataFromDomStorage('memberPosition', 'session') || []);
-
-			if (!urban.mapContainer.id || (positionsList.length < 1)){
-				return false;
-			}
-			
-			// Initializes a OSM Map with options
-			var map = L.map(urban.mapContainer.id).setView(positionsList[0], 15);
-			urban.mapContainer.classList.add("map");
-			// Loads the OSM tileLayer (map background) 
-			L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
-				maxzoom:19,
-				attribution:'(c)<a href="http://www.openstreetmap.org/copyright">OpenStreetMap contributors</a>'
-			}).addTo(map);
-			
-			// Add a marker to each identified position onto the map
-			for(let i = 0, c = positionsList.length ; i < c ; ++i){
-				L.marker(positionsList[i]).addTo(map);
-			}
-			return true;
-		},
-*/
 
 		/********** Functions to get the visitor's current position *********/
 
@@ -460,18 +339,6 @@ if(navigator.geolocation){
 	};
 */
 
-	/****************** MAIN CODE ***************************
-	if(urban.mapTrigger){
-		//urban.mapTrigger.addEventListener('click',urban.showCurrentPosition);
-
-		urban.mapTrigger.addEventListener('click', e => {
-				// urban.showOSMMapBis(); ;
-				let coords = [{long : 4.8325000, lat : 45.7577000}];
-				urban.setOSMMap(coords);
-			}
-		);
-	}
-	*/
 
 }));
 
